382995352d54efac90377f3f4d4e4c4ae4f74f3b
MrHiTech123 <mlebowitz97@gmail.com>
2025-07-19 00:25:04 -0400

    Added ability to get table and save to CSV

---
+++ install.py
@@ -0,0 +1,4 @@
+import os
+
+os.system('pip install selenium')
+os.system('pip install pandas')
--- searcher.py
+++ searcher.py
@@ -1,19 +1,25 @@
 from selenium import webdriver
+import pandas as pd
 from time import sleep
+
+DELAY_SECONDS_AFTER_REQUEST = 1
 
 options = webdriver.FirefoxOptions()
 options.add_argument('--headless')
 driver = webdriver.Firefox(options)
 
-def html_from_url(url: str, delay_seconds: float=0) -> str:
+def html_from_url(url: str) -> str:
     driver.get(url)
-    sleep(delay_seconds)
+    sleep(DELAY_SECONDS_AFTER_REQUEST)
     return driver.page_source
 
+def table_from_url(url: str) -> pd.DataFrame:
+    return pd.read_html(html_from_url(url))[0]
+
 if __name__ == '__main__':
-    x = html_from_url(
-        'https://rensselaercounty.prosgar.com/PROSSearch/SearchIndex?municipality=3814&FilterWaterfronts=False',
-        1
+    x = table_from_url(
+        'https://rensselaercounty.prosgar.com/PROSSearch/SearchIndex?municipality=3814&FilterWaterfronts=False'
     )
-    print(x)
+    x.to_csv('data/test.csv')
+    
     driver.quit()
